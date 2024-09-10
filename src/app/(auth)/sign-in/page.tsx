d9ef929e29925972dd1c27b30691331eee80f3f8
ebad <ebadmuhammad003@gmail.com>
2024-09-10 16:29:46 +0500

    commit

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,16 +1,115 @@
-import React from 'react'
+'use client'
+import React, { useRef, useState } from 'react'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { signInSchema } from '@/schemas/signInSchema'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { any, z } from 'zod'
+import { Loader2 } from 'lucide-react'
+import { Input } from '@/components/ui/input'
+import { signIn } from 'next-auth/react'
+import { useToast } from '@/hooks/use-toast'
+import { useRouter } from 'next/navigation'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
-const signIn = () => {
+const signin = () => {
+  const router = useRouter()
+  const { toast } = useToast()
+  const [isSubmitting, setisSubmitting] = useState(false)
+  const ref=useRef<any>(null)
+
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: '',
+      password: ''
+    }
+  })
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    setisSubmitting(true)
+    const result = await signIn('credentials', {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password
+    })
+    if (result?.error) {
+      toast({
+        title: 'Login failed',
+        description: 'Incorrect username or password',
+        variant: 'destructive'
+      })
+    }
+    setisSubmitting(false)
+    if (result?.url) {
+      router.replace('/dashboard')
+    }
+  }
+
   return (
-    <div className='flex justify-center items-center min-h-screen bg-gray-100'>
-      <div className='w-full max-w-md shadow-lg p-8 space-y-8 bg-white rounded-lg'>
+    <div className='flex justify-center items-center min-h-screen bg-gray-300'>
+      <div className='w-full text-white max-w-sm shadow-lg p-10 space-y-8 bg-[#10103f] rounded-lg'>
         <div className='text-center'>
-          <h1 className="text-3xl font-extrabold tracking-tight lg:text-4xl mb-2">Sign in to nextify</h1>
-          <p className='mb-4'>Welcome back! Please sign in to continue</p>
+          <h1 className="text-2xl font-semibold tracking-tight lg:text-3xl mb-2">Sign in to nextify</h1>
+          <p className='text-sm mb-4 text-[#FFFFFFA6]'>Welcome back! Please sign in to continue</p>
+        </div>
+        <Form {...form} >
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email or Username *</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your username or email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password *</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="Enter your password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex justify-center items-center">
+              <Button type="submit" disabled={isSubmitting} className=' disabled:opacity-55'>
+                {isSubmitting ? (
+                  <>
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait
+                  </>
+                ) : ('Sign in')}
+              </Button>
+            </div>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Don't have an account?{' '}
+            <Link href='/sign-up' className="text-blue-600 hover:text-blue-800">Register here</Link>
+          </p>
         </div>
       </div>
-      </div>
+    </div>
   )
 }
 
-export default signIn
+export default signin
